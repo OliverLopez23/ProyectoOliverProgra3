d43462624fabaa35b27f2ab9db679deb10066138
OliverLopez23 <blopezs22@miumg.edu.gt>
2025-06-13 21:25:51 -0600

    Delete src/proyectoprogra3 directory

--- src/proyectoprogra3/ProyectoProgra3.java
+++
@@ -1,8 +0,0 @@
-package proyectoprogra3;
-
-public class ProyectoProgra3 {
-    public static void main(String[] args) {
-        RegistroVehiculosGT Practicadelist = new RegistroVehiculosGT();
-        Practicadelist.setVisible(true);   
-    }
-}
